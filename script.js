91b6f1b0e2b4ca559b75fe548be658bae4e0f2ff
mnye7 <mnye19@att.net>
2018-05-16 15:31:43 -0400

    working attack and quit button, need to figure out health button fix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 class Computer {
   constructor(name, health) {
     this.name = name;
@@ -8,7 +6,6 @@ class Computer {
   generateAttackDamage() {
     return getRandom(5);
   }
-
 }
 
 class Character extends Computer {
@@ -26,58 +23,129 @@ class Character extends Computer {
   }
 }
 
+function getRandom(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+var user;
+var grant;
+var createBtn = document.getElementById('create');
+var userInput = document.querySelector('input');
+var gameStatus = document.querySelector('.game-status');
+
+/*** Start button ***/
+// startBtn.addEventListener('click', function(){
+//   startWrapper.classList.add('hidden');
+//   userwrapper.classList.remove('hidden');
+//   var userName = userInput.value;
+//   startCombat(userName);
+// });
 var startButton = document.getElementById("startButton");
-startButton.onclick = function(){
+startButton.onclick = function() {
   document.getElementById("game-wrapper").style.display = "block";
   document.getElementById("start-wrapper").style.display = "none";
   startGame();
 }
 
 
+/** start game **/
 
 function startGame() {
-  // var play = prompt('do you want to play?');
-  // if (play.toLowerCase() === 'yes') {
-    var userName = prompt('enter your name');
-    startCombat(userName);
-    // setName(userName);
-    // else {
-    // console.log('Game Over');
+  var userName = prompt('enter your name');
+  startCombat(userName);
+  getGameStatus();
 }
 
-var userName = document.getElementById("userName");
-function setName(userName){
-  playerName.innerText = userName;
+var userName = document.getElementById(".user-name");
+
+function setName(userName) {
+  userName.innerText = userName;
 }
 
+/** start combat **/
+function getGameStatus(){
+  var status = `${user.name} has ${user.health} health remaining Grant has ${grant.health} health remaining`;
+  gameStatus.innerHtml = status;
+}
 
 function startCombat(userName) {
-  var user = new Character(userName, 40, 2);
-  var grant = new Computer('grant', 10);
+
+  user = new Character(userName, 40, 2);
+  grant = new Computer('grant', 10);
   setName(user);
   var attackButton = document.getElementById("attackButton");
   attackButton.onclick = attack;
-  function attack(){
-    if (attack === 'attack'){
-        user.health -= grant.generateAttackDamage();
-        grant.health -= user.generateAttackDamage();
-        console.log(`${user.name} has ${user.health} health remaining`);
-        console.log(`Grant has ${grant.health} health remaining`);
 
-        var playerHealthProgressBar = document.getElementsByClassName("playerHealth")[0]
-        playerHealthProgressBar.value = user.health;
-      }
+  function attack() {
+      user.health -= grant.generateAttackDamage();
+      grant.health -= user.generateAttackDamage();
+      console.log(`${user.name} has ${user.health} health remaining`);
+      console.log(`Grant has ${grant.health} health remaining`);
 
+      var playerHealthProgressBar = document.getElementsByClassName("playerHealth")[0]
+      playerHealthProgressBar.value = user.health;
+
+      var enemyHealthProgressBar = document.getElementsByClassName("enemyHealth")[0];
+      enemyHealthProgressBar.value = grant.health;
+
+
+
+function checkForVictory (){
     if (grant.health < 1) {
-        user.wins++;
-        grant.health = 10;
-        var message = `${user.name} has 1 victory`;
-      }
+      user.wins++;
+      grant.health = 10;
+      var message = `${user.name} has 1 victory`;
+    }
     if (user.wins >= 1) {
       message = `${user.name} has ${user.wins} victories`;
       console.log(message);
-      }
+
+    }
+    }
   }
+
+  /** heal button **/
+
+  var healButton = document.getElementById("healButton");
+  healButton.onclick = heal();
+
+  function heal() {
+    user.Health += getRandom(10);
+    user.healsRemaining--;
+    console.log(`the user has healed and has ${user.health}`);
+  }
+
+    if(user.healsRemaining)
+      user.heal();
+      grant.health -= user.generateAttackDamage();
+      // var playerHealCountProgressBar = document.getElementsByClassName("healCount")[0]
+      var playerHealCountProgressBar = document.getElementsByClassName("healCount");
+
+      playerHealCountProgressBar.value = user.heal;
+
+
+
+
+  if (user.wins === 5) {
+    console.log(`The user has ${user.wins} wins`);
+  }
+}
+  // } else {
+  //   console.log('Grant Wins');
+  // }
+
+
+  /** quit game **/
+  var quitButton = document.getElementById("quitButton");
+  quitButton.onclick = quit;
+
+
+
+  function quit() {
+    console.log("You have quit the game.")
+    return;
+  }
+
 
 
   // while (user.wins < 5 && user.health > 0) {
@@ -115,40 +183,27 @@ function startCombat(userName) {
   //
   // }
 
-  if (user.wins === 5) {
-    console.log(`${user.name} won`);
-  } else {
-    console.log('Grant won');
-  }
-}
+  //   if (user.wins === 5) {
+  //     console.log(`${user.name} won`);
+  //   } else {
+  //     console.log('Grant won');
+  //   }
+  // }
 
 
-// var startButton = document.getElementById("startButton");
-// startButton.onclick = function () {
-//   document.getElementById("game-wrapper").style.display = "block";
-//   document.getElementById("start-wrapper").style.display = "none";
-//   startGame();
-// }
-
-
-// var startButton = document.getElementById("startButton");
-// startButton.onclick = startGame(){
-//   // startGame();
-//   document.getElementById("game-wrapper").style.display = "block";
-//   document.getElementById("start-wrapper").style.display = "none";
-// }
+  // var startButton = document.getElementById("startButton");
+  // startButton.onclick = function () {
+  //   document.getElementById("game-wrapper").style.display = "block";
+  //   document.getElementById("start-wrapper").style.display = "none";
+  //   startGame();
+  // }
 
 
 
 
+  // else {
+  //   console.log('Game not started, you must type "yes" to begin the game.');
+  // }
 
-function getRandom(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
-
-// else {
-//   console.log('Game not started, you must type "yes" to begin the game.');
-// }
-
-// getRandom();
-// startCombat();
+  // getRandom();
+  // startCombat();
